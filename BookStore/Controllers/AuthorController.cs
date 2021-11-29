929599e9de37042a21f8869bea825ab862aa7263
Ericson <Ericson.id@gmail.com>
2021-11-28 22:06:07 -0300

    Routes MVC

---
+++ BookStore/Controllers/AuthorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    [RoutePrefix("autor")]
+    public class AuthorController : Controller
+    {
+        // GET: Autor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Route("editar/{id:int}")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        [Route("excluir/{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
